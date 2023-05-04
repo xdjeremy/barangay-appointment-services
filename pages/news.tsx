5a11000b269bcd0878f34a6b0342120c156a5d55
Tammy <tam.rivera05@gmail.com>
2023-05-04 16:38:05 +0800

    feat: add layout

---
+++ pages/news.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "@/components/layout";
+import NewsPage from "@/components/news.page";
+
+const News = () => {
+    return (
+        <Layout>
+            <NewsPage />
+        </Layout>
+    );
+};
+
+export default News;
