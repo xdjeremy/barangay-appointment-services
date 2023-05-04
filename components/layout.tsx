cf2cb165d3bff9e0f3af2e2d200829a7927935f3
Jeremy <jeremy.fran@icloud.com>
2023-05-04 20:26:45 +0800

    fix: active navbar on layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,13 +2,19 @@ import React, {FC, ReactNode} from 'react';
 import {XMarkIcon} from "@heroicons/react/24/solid";
 import pic from "@/assets/images/kineme.jpg";
 import Image from "next/image";
+import {classNames} from "@/utils";
+import Link from "next/link";
 
 
 interface Props {
     children: ReactNode
+    activePage: string;
 }
 
 const navLinks = [{
+    title: 'News',
+    href: '/news'
+},{
     title: 'Profile',
     href: '/profile'
 }, {
@@ -29,16 +35,15 @@ const navLinks = [{
 }]
 
 
-const NavItem = ({title}: { title: string }) => {
+const NavItem = ({title, href, active}: { title: string, href: string, active: boolean }) => {
     return (
-        <div>
-            <button className={"bg-green-700 text-black w-72 text-3xl text-left px-3.5 py-3"}>
-                {title}
-            </button>
-        </div>
+        <Link href={href}
+              className={classNames(active ? 'bg-white' : 'bg-green-700', 'text-black w-72 text-3xl text-left px-3.5 py-3')}>
+            {title}
+        </Link>
     )
 }
-const Layout: FC<Props> = ({children}) => {
+const Layout: FC<Props> = ({children, activePage}) => {
     return (
         <div className={"bg-white flex flex-row min-h-screen w-full"}>
             <nav className={"flex flex-col bg-[#A2BD96] justify-between w-[480px]"}>
@@ -47,24 +52,20 @@ const Layout: FC<Props> = ({children}) => {
                         B.A.S
                     </div>
                     <div className={"flex flex-col gap-10 items-end"}>
-                        <div>
-                            <button className={"bg-white text-black w-72 text-3xl text-left px-2 py-2"}>
-                                News
-                            </button>
-                        </div>
-
                         {
                             navLinks.map((link) => {
                                 return (
-                                    <NavItem key={link.title} title={link.title}/>
+                                    <NavItem href={link.href} active={activePage === link.title} key={link.title}
+                                             title={link.title}/>
                                 )
                             })
                         }
                     </div>
                 </div>
                 <div className={"pb-5"}>
-                    <button className={"flex flex-row justify-between bg-green-700 text-black w-80 text-xl text-right px-3.5 py-3"}>
-                            <XMarkIcon className={"h-10 w-10 flex-shrink-0"}/>
+                    <button
+                        className={"flex flex-row justify-between bg-green-700 text-black w-80 text-xl text-right px-3.5 py-3"}>
+                        <XMarkIcon className={"h-10 w-10 flex-shrink-0"}/>
                         <div className={"pt-1 pl-5"}>
                             sample.brgy1203
                         </div>
