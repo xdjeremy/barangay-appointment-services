4e9a10ef8c5f7db33ef46d651d7650d928954b5f
Tammy <tam.rivera05@gmail.com>
2023-05-04 09:52:18 +0800

    feat: add register

---
+++ pages/register.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RegisterPage from "@/components/register.page";
+
+const Register = () => {
+    return (
+       <RegisterPage/>
+    );
+};
+
+export default Register;
