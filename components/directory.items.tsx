ba499087df51cada7054b1de4ce27bcfd82228ee
Tammy <tam.rivera05@gmail.com>
2023-05-11 21:47:43 +0800

    refactor directory page to new images

--- components/directory.items.tsx
+++ components/directory.items.tsx
@@ -9,15 +9,18 @@ interface Props {
 const DirectoryItems: FC<Props> = ({name, title, image}) => {
     return (
         <div className={"gap-5 justify-between pt-8 w-full"}>
-            <div className={"bg-gray-200 rounded-lg w-full h-24 flex flex-row px-6 py-2.5 relative"}>
+            <div className={"bg-gray-200 rounded-lg w-full h-24 flex flex-row px-6 py-2.5"}>
                 <div>
-                    <Image src={image} alt={title} className={"rounded-full object-cover w-[75px] h-[75px]"} width={75} height={75}/>
+                    <Image src={image} alt={title} className={"rounded-full object-cover w-[75px] h-[75px]"}
+                           width={75} height={75}/>
                 </div>
-                <div className={"text-black text-3xl pl-20 pt-3"}>
-                    {name}
-                </div>
-                <div className={"absolute bottom-0 right-3 text-black text-xl"}>
-                    {title}
+                <div className={"flex flex-col pl-20"}>
+                    <div className={"text-center text-black text-3xl pt-1"}>
+                        {name}
+                    </div>
+                    <div className={"text-center text-black text-xl"}>
+                        {title}
+                    </div>
                 </div>
             </div>
         </div>
--- components/directory.page.tsx
+++ components/directory.page.tsx
@@ -1,31 +1,44 @@
 import React, {FC} from 'react';
 import DirectoryItems from "@/components/directory.items";
-import pic1 from "../assets/images/kineme 1.jpg"
-import pic2 from "../assets/images/kineme2.jpg"
-import pic3 from "../assets/images/kineme 3.jpg"
-import pic4 from "../assets/images/kineme 4.jpg"
-import pic5 from "../assets/images/kineme 5.jpg"
-import pic6 from "../assets/images/kineme 6.jpg"
-import pic7 from "../assets/images/kineme 7.jpg"
-import pic8 from "../assets/images/kineme 8.png"
-import pic9 from "../assets/images/kineme 9.jpg"
+import pic1 from "../assets/images/img1.png";
+import pic2 from "../assets/images/img2.png";
+import pic3 from "../assets/images/img3.png";
+import pic4 from "../assets/images/img4.png";
+import pic5 from "../assets/images/img5.png";
+import pic6 from "../assets/images/img6.png";
+import pic7 from "../assets/images/img7.png";
+import pic8 from "../assets/images/img8.png";
+import pic9 from "../assets/images/img9.png";
+import pic10 from "../assets/images/img10.png";
+
+
 
 const DirectoryPage: FC = () => {
     return (
         <div>
-            <h1 className={'text-5xl text-black font-bold text-right px-4 pt-3.5 py-12 '}>
+            <h1 className={'text-5xl text-black font-bold text-right px-4 pt-3.5 pb-6'}>
                 BARANGAY OFFICIALS
             </h1>
-            <div className={'grid grid-cols-3 justify-items-center gap-3 pl-10 pr-4'}>
-            <DirectoryItems name={'Kap. Ruth Ancheta'} title={'Barangay Captain'} image={pic1}/>
-            <DirectoryItems name={'Maricel Lejas'} title={'Barangay Secretary'} image={pic2}/>
-            <DirectoryItems name={'Melvin Torrente'} title={'Kagawad'} image={pic3}/>
-            <DirectoryItems name={'Romeo David'} title={'Kagawad'} image={pic4}/>
-            <DirectoryItems name={'Omak David'} title={'Kagawad'} image={pic7}/>
-            <DirectoryItems name={'Eddie Arinal'} title={'Kagawad'} image={pic8}/>
-            <DirectoryItems name={'Allan Miyerkules'} title={'Desk Staff'} image={pic5}/>
-            <DirectoryItems name={'Dough Dimmadome'} title={'Janitor'} image={pic6}/>
-            <DirectoryItems name={'Rosie Rose Rune'} title={'Office'} image={pic9}/>
+            <div className={"border border-t-4 border-b-0 border-l-0 border-r-0 border-[#F3F3F4]"}/>
+            <div className={'justify-center mx-auto w-2/5'}>
+            <DirectoryItems name={'Ruth Esther Torrente-Ancheta'} title={'Barangay Captain'} image={pic1}/>
+            </div>
+            <div className={'grid grid-cols-3 justify-items-center text-center gap-3 pl-10 pr-4 pb-28'}>
+                <DirectoryItems name={'Jasper M. Angeles'} title={'SK Chairman'} image={pic10}/>
+                <DirectoryItems name={'Maricel A. Lejas'} title={'Secretary'} image={pic2}/>
+                <DirectoryItems name={'Arcya Syren B. Bolano'} title={'Treasurer'} image={pic3}/>
+            </div>
+            <h1 className={'text-5xl text-black font-bold text-right px-4 py-6'}>
+                BARANGAY STAFFS
+            </h1>
+            <div className={"border border-t-4 border-b-0 border-l-0 border-r-0 border-[#F3F3F4]"}/>
+            <div className={'grid grid-cols-3 justify-items-center text-center gap-3 pl-10 pr-4'}>
+                <DirectoryItems name={'Eduardo V. Arinal'} title={'Kagawad'} image={pic4}/>
+                <DirectoryItems name={'Ericson B. Bolanio'} title={'Kagawad'} image={pic5}/>
+                <DirectoryItems name={'Fidel P. Olarte Jr.'} title={'Kagawad'} image={pic6}/>
+                <DirectoryItems name={'Melvin N. Torrente Jr'} title={'Kagawad'} image={pic7}/>
+                <DirectoryItems name={'Omar Friedrich L. David'} title={'Kagawad'} image={pic8}/>
+                <DirectoryItems name={'Romeo V. David'} title={'Kagawad'} image={pic9}/>
             </div>
         </div>
 
